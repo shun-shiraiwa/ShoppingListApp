8b1d4aca3bddba3b19f3e22bb73f3538c22d0d52
shun-shiraiwa <shun.shiraiwa1999@gmail.com>
2025-02-14 03:43:21 +0900

    プロジェクトの新規作成

---
+++ src/ShoppingListApp.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ShoppingListApp extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
